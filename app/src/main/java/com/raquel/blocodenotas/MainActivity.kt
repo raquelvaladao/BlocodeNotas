a49b5b4b0aa9b1d75f4715b7db4742e20e3feaeb
donathh <raqueldepapel@gmail.com>
2021-05-29 18:00:56 -0300

    gradle added

---
+++ app/src/main/java/com/raquel/blocodenotas/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.raquel.blocodenotas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
